db020bfe65b147068b1d70c334410ca76f3de716
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-12-21 23:02:46 -0500

    made a few changes, added sass

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let cars = [
-  {
+//Created an array of objects with car info.
+let cars = [{
     make: "Honda",
     model: "CRV",
     spaces: 1,
@@ -36,6 +36,7 @@ let cars = [
     money: 53
 }]
 
+//Wrote function that will loop through car objects and create elements to render HTML.
 function init() {
     let parent = document.querySelector('.cars');
     for (let i = 0; i < cars.length; i++) {
@@ -57,15 +58,14 @@ function init() {
         let button2 = document.createElement("button");
         let button3 = document.createElement("button");
 
+//After creating elements, added event listeners on each button to post for each Lot.
+
         const URI = 'https://quiet-reef-60052.herokuapp.com/park';
-        // const URI = "http://192.168.1.91:4567/park";
 
         button0.addEventListener('click', function() {
             let request = new XMLHttpRequest();
             console.log('Soon to post to LOT 0 ' + URI);
-
             request.open('POST', URI);
-
             let body = JSON.stringify({
                 make: cars[i].make,
                 model: cars[i].model,
@@ -73,9 +73,7 @@ function init() {
                 money: cars[i].money,
                 id: 0
             });
-
             request.send(body);
-
         });
 
         button1.addEventListener('click', function() {
@@ -84,15 +82,13 @@ function init() {
 
             request.open('POST', URI);
             let body = JSON.stringify({
-              make: cars[i].make,
-              model: cars[i].model,
-              spaces: cars[i].spaces,
-              money: cars[i].money,
-              id: 1
-              });
-
-              request.send(body);
-
+                make: cars[i].make,
+                model: cars[i].model,
+                spaces: cars[i].spaces,
+                money: cars[i].money,
+                id: 1
+            });
+            request.send(body);
         });
 
         button2.addEventListener('click', function() {
@@ -101,15 +97,13 @@ function init() {
 
             request.open('POST', URI);
             let body = JSON.stringify({
-              make: cars[i].make,
-              model: cars[i].model,
-              spaces: cars[i].spaces,
-              money: cars[i].money,
-              id: 2
-              });
-
-              request.send(body);
-
+                make: cars[i].make,
+                model: cars[i].model,
+                spaces: cars[i].spaces,
+                money: cars[i].money,
+                id: 2
+            });
+            request.send(body);
         });
 
         button3.addEventListener('click', function() {
@@ -118,14 +112,13 @@ function init() {
 
             request.open('POST', URI);
             let body = JSON.stringify({
-              make: cars[i].make,
-              model: cars[i].model,
-              spaces: cars[i].spaces,
-              money: cars[i].money,
-              id: 3
-              });
-
-              request.send(body);
+                make: cars[i].make,
+                model: cars[i].model,
+                spaces: cars[i].spaces,
+                money: cars[i].money,
+                id: 3
+            });
+            request.send(body);
 
         });
 
@@ -133,7 +126,7 @@ function init() {
         div.appendChild(carMakeModel);
         div.appendChild(carSpaces);
         div.appendChild(money);
-        body.appendChild(btndiv);
+        div.appendChild(btndiv);
         btndiv.appendChild(button0);
         btndiv.appendChild(button1);
         btndiv.appendChild(button2);
@@ -152,59 +145,79 @@ function init() {
         button2.className = "button";
         button3.className = "button";
     }
-
-
 }
 
-function availableParking(){
-  const URI = 'https://quiet-reef-60052.herokuapp.com/lot';
 
-  let request = new XMLHttpRequest();
+//Wrote GET request to get the information from the heroku url.
+function availableParking() {
+    const URI = 'https://quiet-reef-60052.herokuapp.com/lot';
 
-  request.open('GET', URI);
+    let request = new XMLHttpRequest();
 
-  request.addEventListener('load', function(){
-    let response = JSON.parse(request.responseText);
-    console.log(response);
+    request.open('GET', URI);
 
-    let spaceAvailable = response;
-    console.log(spaceAvailable);
+    request.addEventListener('load', function() {
+        let response = JSON.parse(request.responseText);
+        console.log(response);
 
-    showLots(spaceAvailable);
+        console.log(response[0].parkedCars.make);
+        console.log(response);
+        showLots(response);
 
-  });
+        let car = response[0].parkedCars.make;
 
-  function showLots(lot) {
-      let list = document.querySelector("ul");
+    });
+
+    //Wrote function that is called in the GET function, that will create HTML elements rendering the information I am GETting from backend.
+    function showLots(lot) {
+        let list = document.querySelector("ul");
 
         for (let i = 0; i < lot.length; i++) {
-          let lotid = document.createElement("h3");
-          let capacity = document.createElement("h3");
-          let cost = document.createElement("h3");
-          let name = document.createElement("h4");
-          let listdiv = document.createElement("div");
-          listdiv.setAttribute("class", "listdiv");
+            let lotid = document.createElement("h3");
+            let capacity = document.createElement("h3");
+            let cost = document.createElement("h3");
+            let name = document.createElement("h4");
+            let listdiv = document.createElement("div");
+            listdiv.setAttribute("class", "listdiv");
 
-          lotid.textContent = "Lot #" + lot[i].id;
-          capacity.textContent = "Capacity: " + lot[i].capacity;
-          cost.textContent = "Cost: $" + lot[i].rate;
-          name.textContent = "Name of car: " + lot[i].parkedCars;
+            // let spacesLeft = lot[i].capacity - cars[i].spaces
 
+            lotid.textContent = "Lot #" + lot[i].id;
+            capacity.textContent = "Capacity: " + lot[i].capacity + "/" + lot[i].totalSpaces;
+            cost.textContent = "Cost: $" + lot[i].rate;
+            name.textContent = "Name of car: " + lot[i].parkedCars[i].make + " " + lot[i].parkedCars[i].model;
 
-          list.appendChild(listdiv);
-          listdiv.appendChild(lotid);
-          listdiv.appendChild(capacity);
-          listdiv.appendChild(cost);
-          listdiv.appendChild(name);
-       }
+            list.appendChild(listdiv);
+            listdiv.appendChild(lotid);
+            listdiv.appendChild(capacity);
+            listdiv.appendChild(cost);
+            listdiv.appendChild(name);
 
-  }
+            // //trying to loop here to make the names of the cars show...i think i need to push the parked cars to an array and display..
+            // let parkedCars = lot[i].parkedCars;
+            // function parkIt(parkedCars) {
+            //   for (let j = 0; i < parkedCars.length; j++) {
+            //     name.textContent = "Name of car: " + parkedCars[j].make;
+            //   }
+            // }
+            // //calling above function
+            // parkIt(parkedCars);
 
-  request.send();
+            //trying to set interval to allow count to update, instead of having to refresh page. I COULD just allow the button to reset the page but thats cheating?
+            // setInterval(function update(){
+            //   capacity.textContent = "Capacity: " + spacesLeft + "/" + lot[i].totalSpaces;
+            // }, 2000)
+
+        }
+    }
+    request.send();
 }
 
+//Questions:
+//1) Allowing capacity to update without refreshing page.
+//2) Having the car name display, once its parked in the lot. Its showing up as Object, object, and I cant get the name. I can only get it written in the console.
 
 
-// setInterval(availableParking, 2000);
+
 window.addEventListener('load', init);
 window.addEventListener('load', availableParking);
